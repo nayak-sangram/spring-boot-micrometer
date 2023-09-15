d7f4f174b29bb61f2f0cfe33f580e55f7b9af246
abc <abc@example.com>
2023-09-14 22:31:08 -0400

    add

---
+++ src/test/java/com/spring/monitoring/SpringBootMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
